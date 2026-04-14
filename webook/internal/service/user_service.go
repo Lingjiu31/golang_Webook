6e2656cd0808a94c89865be373a41074654a548d
LIngJiu31 <shz_1018@163.com>
2026-04-14 22:37:51 +0800

    增加了密码加密

--- webook/internal/service/user_service.go
+++ webook/internal/service/user_service.go
@@ -4,6 +4,8 @@ import (
 	"Project-WeBook/webook/internal/domain"
 	"Project-WeBook/webook/internal/repository"
 	"context"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 type UserService struct {
@@ -17,5 +19,12 @@ func NewUserService(repo *repository.UserRepository) *UserService {
 }
 
 func (svc *UserService) SignUp(cxt context.Context, user domain.User) error {
+	//加密
+	hash, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+	user.Password = string(hash)
+
 	return svc.repo.Create(cxt, user)
 }
