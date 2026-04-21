c03b3c8dc124c298b079d4993d23db0e535d5cde
LIngJiu31 <shz_1018@163.com>
2026-04-21 22:20:18 +0800

    refactor:session改为使用redis

--- webook/internal/middleware/session.go
+++ webook/internal/middleware/session.go
@@ -2,11 +2,18 @@ package middleware
 
 import (
 	"github.com/gin-contrib/sessions"
-	"github.com/gin-contrib/sessions/cookie"
+	"github.com/gin-contrib/sessions/redis"
 	"github.com/gin-gonic/gin"
 )
 
 func SessionMiddleware() gin.HandlerFunc {
-	store := cookie.NewStore([]byte("secret"))
+	//store := cookie.NewStore([]byte("secret"))
+	store, err := redis.NewStore(16, "tcp",
+		"localhost:6379", "", "",
+		[]byte("h9X2&kL#vR6wP*zY4nM1jT8fQ5sC7aE9"),
+		[]byte("h9X2&kL#vR6wP*zY4nM1jT8fQ5sC7aE9"))
+	if err != nil {
+		panic(err)
+	}
 	return sessions.Sessions("mysession", store)
 }
